a82f8a23258cb69f52e1d7d05574dd24f264ff7e
Daniel Heppner <daniel.heppner@ibigroup.com>
2024-07-02 14:25:50 +0100

    use bun + devenv

---
+++ devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  languages.javascript.enable = true;
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.bun pkgs.k6 ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "k6";
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
